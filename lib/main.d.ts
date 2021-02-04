2470e1808b4d4d655cef4cd42317e09cc234c822
DuckyYang <duckyyang@vip.qq.com>
2021-02-04 12:24:59 +0800

    complete v1.0

--- lib/main.d.ts
+++
@@ -1,4 +0,0 @@
-import { RoutePrefix, Get, Post, Put, Delete, Patch, Path, Query, Body, Header, Cookie } from "./route-decorator";
-import RouteInterceptor from "./route-interceptor";
-import { RouteMeta, RouteMethodMeta, RouteParamMeta } from "./route-meta";
-export { RoutePrefix, Get, Post, Put, Delete, Patch, Path, Query, Body, Header, Cookie, RouteInterceptor, RouteMeta, RouteMethodMeta, RouteParamMeta };
